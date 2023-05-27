cd53eecc669eea25e75bc90e48cc97eea7880227
bhasevambit <jammadoibit6123@gmail.com>
2023-05-27 15:49:29 +0900

    Update tsx files

--- react-sample/src/index.tsx
+++ react-sample/src/index.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+
 import App from './App';
+import Hello from './components/Hello';  // 自作したHelloモジュールをimport
+
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
@@ -9,6 +12,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
+    <Hello />
     <App />
   </React.StrictMode>
 );
