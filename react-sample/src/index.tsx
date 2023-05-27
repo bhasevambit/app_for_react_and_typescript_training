13efd00700a200c9cd272f7ea07ac0dd4d760eba
bhasevambit <jammadoibit6123@gmail.com>
2023-05-27 16:26:06 +0900

    Update several tsx files

--- react-sample/src/index.tsx
+++ react-sample/src/index.tsx
@@ -1,18 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
-import App from './App';
-import Hello from './components/Hello';  // 自作したHelloモジュールをimport
+import App from "./App";
+import Hello from "./components/Hello"; // 自作した Hello モジュールをimport
+import Name from "./components/Name"; // 自作した Name モジュールをimport
+import Message from "./components/Message"; // 自作した Message モジュールをimport
+import Page from "./components/ContextSample"; // 自作した Page モジュールをimport
 
-import reportWebVitals from './reportWebVitals';
+import Parent from "./components/MemoSample1"; // 自作した Parent モジュールをimport
+
+import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
     <Hello />
+    <hr></hr>
+    <Name />
+    <hr></hr>
+    <Message />
+    <hr></hr>
+    <Page />
+    <hr></hr>
+    <Parent />
+    <hr></hr>
     <App />
   </React.StrictMode>
 );
